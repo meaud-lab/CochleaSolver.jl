8307f9d6766ec4dde1267b787aaf367c061db440
Rouleau, Michael <44850854+mikerouleau@users.noreply.github.com>
2022-05-14 15:03:46 -0400

    parameter type naming consistency

--- src/excitation/gaussianenvelope.jl
+++ src/excitation/gaussianenvelope.jl
@@ -1,22 +1,22 @@
-struct GaussianEnvelope{T1,T2,T3,T4}
+struct GaussianEnvelopeParams{T1,T2,T3,T4}
     t0::T1
     sigma::T2
     F0::T3
     v::T4
-    function GaussianEnvelope(t::T1, s::T2, f::T3, v::T4) where {T1,T2,T3,T4}
+    function GaussianEnvelopeParams(t::T1, s::T2, f::T3, v::T4) where {T1,T2,T3,T4}
         return new{T1,T2,T3,T4}(t, s, f, v)
     end
 end
 
 
-function GaussianEnvelope(d::Dict)
+function GaussianEnvelopeParams(d::Dict)
     t0 = d["t0_GaussEnv"]
     sigma = d["sigma_GaussEnv"]
     F0 = d["F0"]
     v = d["v"]
-    return GaussianEnvelope(t0, sigma, F0, v)
+    return GaussianEnvelopeParams(t0, sigma, F0, v)
 end
 
 function gaussianenv!(V, p, t)
-    V = p.v * exp.(-0.5 * ((t - p.t0) ./ p.sigma) .^ 2 .* sin.(p.F0 * 2π .* t))
+    V .= p.v .* exp.(-0.5 * ((t - p.t0) ./ p.sigma) .^ 2 .* sin.(p.F0 * 2π .* t))
 end
--- src/excitation/twotone.jl
+++ src/excitation/twotone.jl
@@ -1,4 +1,4 @@
-struct TwoToneParameters{T1,T2,T3,T4,T5,T6,T7}
+struct TwoToneParams{T1,T2,T3,T4,T5,T6,T7}
     omega::T1
     mag::T2
     phi::T3
@@ -6,20 +6,20 @@ struct TwoToneParameters{T1,T2,T3,T4,T5,T6,T7}
     tR::T5
     tOn::T6
     t0::T7
-    function TwoToneParameters(omega::T1, mag::T2, phi::T3, force::T4, tR::T5, tOn::T6) where {T1,T2,T3,T4,T5,T6}
+    function TwoToneParams(omega::T1, mag::T2, phi::T3, force::T4, tR::T5, tOn::T6) where {T1,T2,T3,T4,T5,T6}
         t0 = tOn - tR
         return new{T1,T2,T3,T4,T5,T6,typeof(t0)}(omega, mag, phi, force, tR, tOn, t0)
     end
 end
 
-function TwoToneParameters(d::Dict)
+function TwoToneParams(d::Dict)
     force = Vector(undef, 2) #TODO: specify type
     force[1] = d["v"] .* d["Force_correction"][1]
     force[2] = d["v"] .* d["Force_correction"][2]
     mag = map(x -> abs.(x), force)
     phi = map(x -> angle.(x), force)
     omega = [d["omega1"], d["omega2"]]
-    return TwoToneParameters(omega, mag, phi, force, d["tR"], d["tOn"])
+    return TwoToneParams(omega, mag, phi, force, d["tR"], d["tOn"])
 end
 
 function twotone!(V, p, t)
